ed32bb4122dc22a234ad0f869d54c6d7c04f2c2f
Jansen Felipe <jansen.felipe@gmail.com>
2014-12-29 14:59:37 -0200

    local env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,9 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('homestead'),
-
-));
+$env = $app->detectEnvironment(function(){
+    return getenv('LARAVEL_ENV') ?: 'production';
+});
 
 /*
 |--------------------------------------------------------------------------
