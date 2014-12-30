7655277e6bf13a23706b98e77fe80eaa56bef487
Jansen Felipe <jansen.felipe@gmail.com>
2014-12-30 17:50:22 -0200

    Menu

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -13,27 +13,10 @@
 <div class="row">
     <div class="col-lg-12">
 
-        <!-- Faturamento -->
-        <div class="panel panel-warning">
-            <div class="panel-heading">
-                <h3 class="panel-title"><i class="fa fa-upload fa-fw"></i> Importar notas fiscais</h3>
-            </div>
-            <div class="panel-body">
-                <p>Envie os arquivos de suas notas fiscais de entrada e saída e evite inúmeros cadastros!</p>
-
-                <form method="post" action="/notaFiscal/importar" enctype="multipart/form-data" style="display: none;">
-                    <input name="files[]" id="files" type="file" multiple="true" accept="application/xml" />
-                </form>
-
-                <input type="button" id="btnSelecionarArquivos" class="btn btn-success btn-lg" value="Importar arquivos .xml" />
-            </div>
+        <div class="alert alert-info alert-dismissable">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <i class="fa fa-info-circle"></i> Dica: <a href="/notaFiscal" class="alert-link">Clique aqui</a> para importar suas NFe de entrada e saída e evite cadastrar clientes, produtos, fornecedores entre outros! 
         </div>
-
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-lg-12">
 
         <!-- Faturamento -->
         <div class="panel panel-default">
@@ -41,8 +24,8 @@
                 <h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i> Faturamento</h3>
             </div>
             <div class="panel-body">
-                <div class="alert alert-info alert-dismissable">
-                    <i class="fa fa-info-circle"></i> Nenhum pedido foi faturado para exibir no gráfico.
+                <div class="alert alert-warning">
+                    <i class="fa fa-warning"></i> Nenhum pedido foi faturado para exibir no gráfico.
                 </div>
             </div>
         </div>
@@ -60,8 +43,8 @@
                 <h3 class="panel-title"><i class="fa fa-building fa-fw"></i> Estoque de produtos</h3>
             </div>
             <div class="panel-body">
-                <div class="alert alert-info alert-dismissable">
-                    <i class="fa fa-info-circle"></i> Nenhum produto encontrado! <a href="#" class="alert-link">Clique aqui</a> para cadastrar seu primeiro produto!
+                <div class="alert alert-warning">
+                    <i class="fa fa-warning"></i> Nenhum produto encontrado! <a href="#" class="alert-link">Clique aqui</a> para cadastrar seu primeiro produto!
                 </div>
             </div>
         </div>
@@ -76,8 +59,8 @@
                 <h3 class="panel-title"><i class="fa fa-dollar fa-fw"></i> Pedidos</h3>
             </div>
             <div class="panel-body">
-                <div class="alert alert-info alert-dismissable">
-                    <i class="fa fa-info-circle"></i> Nenhum pedido encontrado! <a href="#" class="alert-link">Clique aqui</a> para cadastrar seu primeiro pedido!
+                <div class="alert alert-warning">
+                    <i class="fa fa-warning"></i> Nenhum pedido encontrado! <a href="#" class="alert-link">Clique aqui</a> para cadastrar seu primeiro pedido!
                 </div>
             </div>
         </div>
@@ -89,10 +72,10 @@
 
 @section('javascript')
 <script type="text/javascript">
-    $(function () {
-        $("#btnSelecionarArquivos").click(function () {
+    $(function() {
+        $("#btnSelecionarArquivos").click(function() {
             $("#files").click();
-            $("#files").change(function () {
+            $("#files").change(function() {
                 $(this).parent().submit();
             });
         });
--- app/views/sidebar.blade.php
+++ app/views/sidebar.blade.php
@@ -1,15 +1,70 @@
 <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
 <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav side-nav">
-        <li class="active">
+        <li>
             <a href="/"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-        </li>
+        </li>  
+
         <li>
-            <a href="/cliente"><i class="fa fa-fw fa-users"></i> Clientes</a>
+            <a href="#" data-toggle="collapse" data-target="#cadastros"><i class="fa fa-fw fa-users"></i> Cadastros de Pessoas <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="cadastros" class="collapse">
+                <li>
+                    <a href="/cliente">Clientes</a>
+                </li>
+                <li>
+                    <a href="/fornecedor">Fornecedor</a>
+                </li>
+                <li>
+                    <a href="/funcionario">Funcionários</a>
+                </li>
+            </ul>
         </li>
+
         <li>
-            <a href="/fornecedor"><i class="fa fa-fw fa-table"></i> Fornecedor</a>
-        </li>       
+            <a href="#" data-toggle="collapse" data-target="#financeiro"><i class="fa fa-fw fa-dollar"></i> Financeiro <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="financeiro" class="collapse">
+                <li>
+                    <a href="/notaFiscal">Notas fiscais</a>
+                </li>
+                <li>
+                    <a href="/boleto">Boletos</a>
+                </li>
+                <li>
+                    <a href="/sintegra">Sintegra</a>
+                </li>
+            </ul>
+        </li>
+
+        <li>
+            <a href="#" data-toggle="collapse" data-target="#outrosCadastros"><i class="fa fa-fw fa-gear"></i> Outros cadastros <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="outrosCadastros" class="collapse">
+                <li>
+                    <a href="/produto">Produtos</a>
+                </li>
+                <li>
+                    <a href="/servico">Serviços</a>
+                </li>
+                <li>
+                    <a href="/estoque">Estoque</a>
+                </li> 
+            </ul>
+        </li>
+
+        <li>
+            <a href="#" data-toggle="collapse" data-target="#estoque"><i class="fa fa-fw fa-cubes"></i> Estoque<i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="estoque" class="collapse">
+                <li>
+                    <a href="/estoque/entrada">Entradas</a>
+                </li>
+                <li>
+                    <a href="/estoque/saida">Saídas</a>
+                </li>
+            </ul>
+        </li> 
+
+        <li>
+            <a href="/calendario"><i class="fa fa-fw fa-calendar"></i> Calendário</a>
+        </li>  
     </ul>
 </div>
 <!-- /.navbar-collapse -->
