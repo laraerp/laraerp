2bc860820ca187863d2268321a92bbbc9de65953
Jansen Felipe <jansen.felipe@gmail.com>
2015-01-26 17:56:27 -0200

    mysql pagodabox

--- app/config/pagodabox/database.php
+++ app/config/pagodabox/database.php
@@ -5,10 +5,10 @@ return array(
     'connections' => array(
         'mysql' => array(
             'driver' => 'mysql',
-            'host' => $_SERVER["DB1_HOST"],
-            'database' => $_SERVER["DB1_NAME"],
-            'username' => $_SERVER["DB1_USER"],
-            'password' => $_SERVER["DB1_PASS"],
+            'host' => getenv("DB1_HOST"),
+            'database' => getenv("DB1_NAME"),
+            'username' => getenv("DB1_USER"),
+            'password' => getenv("DB1_PASS"),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
