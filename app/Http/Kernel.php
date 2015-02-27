152d5c4e4ed6a514f35b7424f6cb125403671dfa
Jansen Felipe <jansen.felipe@gmail.com>
2015-02-27 17:51:18 -0300

    Update L5

---
+++ app/Http/Kernel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel {
+
+    public function __construct(\Illuminate\Contracts\Foundation\Application $app, \Illuminate\Routing\Router $router) {
+        parent::__construct($app, $router);
+    }
+
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
+        'Illuminate\Cookie\Middleware\EncryptCookies',
+        'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
+        'Illuminate\Session\Middleware\StartSession',
+        'Illuminate\View\Middleware\ShareErrorsFromSession'
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        'auth' => 'Laraerp\Http\Middleware\Authenticate'
+    ];
+
+}
--- app/controllers/DashboardController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class DashboardController extends BaseController {
-
-    public function getIndex() {
-        return View::make('dashboard');
-    }
-
-}
