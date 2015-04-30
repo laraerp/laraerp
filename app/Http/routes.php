541a9e70e7f6eeed85169919cb46cb36a6a7cd97
Jansen Felipe <jansen.felipe@gmail.com>
2015-04-29 23:56:37 -0300

    Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,4 +1,22 @@
 <?php
 
-//Custom routes
+/*
+ * Models
+ */
+Route::model('cliente', 'Laraerp\Cliente');
+
+Route::group(['middleware' => ['auth']], function() {
+
+    /*
+     * Cliente
+     */
+    Route::get('cliente', ['as' => 'cliente.index', 'uses' => '\Laraerp\Http\Controllers\ClienteController@index']);
+
+
+    /*
+     * Dashboard
+     */
+    Route::get('/', '\Laraerp\Http\Controllers\DashboardController@index');
+});
+
 
