81cd139e8a554dfbeb074f0616b8478e8944ef41
Jansen Felipe <jansen.felipe@gmail.com>
2015-05-01 21:31:24 -0300

    Update route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,10 +8,17 @@ Route::model('cliente', 'Laraerp\Cliente');
 Route::group(['middleware' => ['auth']], function() {
 
     /*
+     * Pessoa
+     */
+    Route::post('pessoa/receitaParams', ['as' => 'pessoa.receitaParams', 'uses' => '\Laraerp\Http\Controllers\PessoaController@receitaParams']);
+    Route::post('pessoa/receitaConsulta', ['as' => 'pessoa.receitaConsulta', 'uses' => '\Laraerp\Http\Controllers\PessoaController@receitaConsulta']);
+
+    /*
      * Cliente
      */
     Route::get('cliente', ['as' => 'cliente.index', 'uses' => '\Laraerp\Http\Controllers\ClienteController@index']);
     Route::get('cliente/form', ['as' => 'cliente.form', 'uses' => '\Laraerp\Http\Controllers\ClienteController@form']);
+    Route::post('cliente/cadastrar', ['as' => 'cliente.cadastrar', 'uses' => '\Laraerp\Http\Controllers\ClienteController@cadastrar']);
 
 
     /*
