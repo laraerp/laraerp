b4a7a1e2b34f7523a6f52dd7c08365d5552ab643
Jansen Vitor <jansen.vitor@sebraemg.com.br>
2015-05-06 18:22:08 -0300

    Update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,7 +20,7 @@ Route::group(['middleware' => ['auth']], function() {
     Route::get('cliente/form', ['as' => 'cliente.form', 'uses' => '\Laraerp\Http\Controllers\ClienteController@form']);
     Route::post('cliente/cadastrar', ['as' => 'cliente.cadastrar', 'uses' => '\Laraerp\Http\Controllers\ClienteController@cadastrar']);
     Route::get('cliente/ver/{cliente}', ['as' => 'cliente.ver', 'uses' => '\Laraerp\Http\Controllers\ClienteController@ver']);
-    Route::post('cliente/editar{cliente}', ['as' => 'cliente.editar', 'uses' => '\Laraerp\Http\Controllers\ClienteController@editar']);
+    Route::post('cliente/editar/{cliente}', ['as' => 'cliente.editar', 'uses' => '\Laraerp\Http\Controllers\ClienteController@editar']);
     Route::get('cliente/deletar/{cliente}', ['as' => 'cliente.deletar', 'uses' => '\Laraerp\Http\Controllers\ClienteController@deletar']);
 
 
