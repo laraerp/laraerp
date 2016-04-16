e91cdb4e7ec64ed960c8f2bbd9ed968ce72ea5fb
chucrute <rafaelpugg@gmail.com>
2016-04-16 18:18:22 -0300

    Tradução Login | Auth Laravel

--- template/resources/views/auth/login.blade.php
+++ template/resources/views/auth/login.blade.php
@@ -12,14 +12,14 @@
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                         <div class="form-group">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
+                            <label class="col-md-4 control-label">E-Mail</label>
                             <div class="col-md-6">
                                 <input type="email" class="form-control" name="email" value="{{ old('email') }}">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-4 control-label">Password</label>
+                            <label class="col-md-4 control-label">Senha</label>
                             <div class="col-md-6">
                                 <input type="password" class="form-control" name="password">
                             </div>
@@ -29,7 +29,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember"> Remember Me
+                                        <input type="checkbox" name="remember"> Lembrar-me
                                     </label>
                                 </div>
                             </div>
